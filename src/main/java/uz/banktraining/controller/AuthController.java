47cd6ecbda21cd7fdc1fc54aef549a3557193f8d
Temur <https://github.com/UNkn0wn1111>
2022-09-14 20:52:10 +0500

    Upload fix

--- src/main/java/uz/banktraining/controller/AuthController.java
+++ src/main/java/uz/banktraining/controller/AuthController.java
@@ -1,21 +1,31 @@
 package uz.banktraining.controller;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.http.codec.multipart.FilePart;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import uz.banktraining.dto.AdminDTO;
 import uz.banktraining.dto.ResponseDTO;
 import uz.banktraining.service.AdminService;
+import uz.banktraining.service.ItemService;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
     private final AdminService adminService;
+    private final ItemService itemService;
 
-    public AuthController( AdminService adminService) {
+
+    public AuthController(AdminService adminService, ItemService itemService) {
 
         this.adminService = adminService;
+        this.itemService = itemService;
     }
 
     @PostMapping("/check")
@@ -24,4 +34,13 @@ public class AuthController {
     }
 
 
+    @PostMapping("/upload")
+    public ResponseDTO uploadFile(@RequestParam("file") MultipartFile file) {
+        try {
+            itemService.save(file);
+            return new ResponseDTO(0, "SUCCESS", null, null);
+        } catch (Exception e) {
+            return new ResponseDTO(1, "ERROR", e.getMessage(), null);
+        }
+    }
 }
