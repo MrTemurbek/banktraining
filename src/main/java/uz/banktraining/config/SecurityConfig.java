039550114b587db9323a0203fb7ac9bf22068afd
Temur <https://github.com/UNkn0wn1111>
2022-10-03 20:27:22 +0500

    cors

--- src/main/java/uz/banktraining/config/SecurityConfig.java
+++ src/main/java/uz/banktraining/config/SecurityConfig.java
@@ -30,6 +30,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
+                .cors()
+                .and()
                 .authorizeRequests()
                 .antMatchers("/auth/check", "/auth/download/**")
                 .permitAll()
